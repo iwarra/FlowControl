2ac4e0005f97c9f5467ecdbf27de1f4bbe7292b4
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-09-12 17:51:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FlowControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Welcome to the main menu. Please select your option: \n0: Exit \n1: Feedback");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0": Environment.Exit(0);
+                        break;
+                    case "1":
+                        Console.WriteLine("Please provide a valid input"); 
+                        break;
+                    default:
+                        break;
+                }
+            }
+            while (true);
+        }
+    }
+}
