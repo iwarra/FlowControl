78d024f6ec767f77b6b6648d243e7ac43660848a
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-09-12 18:36:51 +0200

    Case 1 for different age groups

--- Program.cs
+++ Program.cs
@@ -6,14 +6,23 @@
         {
             do
             {
-                Console.WriteLine("Welcome to the main menu. Please select your option: \n0: Exit");
+                Console.WriteLine("Välkommen till huvudmenyn. Vänligen välj ditt alternativ: \n0: Avsluta \n1: Biljettpris");
                 string input = Console.ReadLine();
                 switch (input)
                 {
                     case "0": Environment.Exit(0);
                         break;
+                        case "1":
+                        Console.Write("Vänligen ange din ålder: ");
+                        // Add logic to check if the input is correct
+                        int age = int.Parse(Console.ReadLine());
+                        if (age > 100 || age < 5) Console.WriteLine("Gratis tillträde");
+                        else if (age < 20) Console.WriteLine("Ungdomspris: 80kr");
+                        else if (age > 64) Console.WriteLine("Pensionärspris: 90kr");
+                        else Console.WriteLine("Standardpris: 120kr");
+                        break;
                     default:
-                        Console.WriteLine("Please provide a valid input");
+                        Console.WriteLine("Vänligen ange ett giltigt alternativ");
                         break;
                 }
             }
