c238b4b58d56855a97057d4adca545c44c96a1ef
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-09-13 20:33:21 +0200

    Added methods for functionality

--- Program.cs
+++ Program.cs
@@ -4,83 +4,105 @@
     {
         static void Main(string[] args)
         {
+            bool isActive = true;
             do
             {   //Turn the menu options to variables and refactor the print menu line to be more readable
                 Console.WriteLine("\nVälkommen till huvudmenyn. Vänligen välj ditt alternativ: \n0: Avsluta \n1: Biljettpris \n3: Lägg till ett meddelande \n4: Skriv minst tre ord");
                 string input = Console.ReadLine();
+
                 switch (input)
                 {
-                    case "0": Environment.Exit(0);
+                    case "0": 
+                        isActive = false;
                         break;
                     case "1":
-                        Console.Write("Vänligen ange din ålder: ");
-
-                        uint age;
-                        string ageInput = Console.ReadLine();
-
-                        //Create a method to validate integers ?
-                        while (!uint.TryParse(ageInput, out age))
-                        {
-                            Console.WriteLine("Vänligen ange korrekt ålder");
-                            ageInput = Console.ReadLine();
-                        }
-
-                        if (age > 100 || age < 5) Console.WriteLine("Gratis tillträde");
-                        else if (age < 20) Console.WriteLine("Ungdomspris: 80kr");
-                        else if (age > 64) Console.WriteLine("Pensionärspris: 90kr");
-                        else Console.WriteLine("Standardpris: 120kr");
-
+                        PrintThePrice(GetUIntInput("ålder"));
                         break;
                     case "2":
                         //Add calculation for the whole company
                         break;
                     case "3":
-                        int printCount = 10;
-                        Console.WriteLine("Vänligen skriv ditt meddelande: ");
-                        string message = Console.ReadLine();
-
-                        while (!IsValidStringInput(message))
-                        {
-                            Console.WriteLine("Vänligen ange ett giltigt meddelande");
-                            message = Console.ReadLine();
-                        }
-
-                        for (int i = 0; i < printCount; i++)
-                        {
-                            Console.Write(message);
-                        }
+                        PrintMessage(GetStringInput("meddelande"), 10);
                         break;
-
                     case "4":
                         int wordCount = 3;
-                        Console.WriteLine($"Vänligen skriv ditt meddelande med minst {wordCount} ord: ");
-                        string messageToSplit = Console.ReadLine();
+                        string messageToSplit = GetStringInput($"meddelande med minst {wordCount} ord: ");
                         //Spliting the user input and adding option in split method that removes empty entries in case of more whitespaces
                         string[] words = messageToSplit.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-                        while (!IsValidStringInput(messageToSplit) || words.Length < 3)
-                        {
-                            Console.WriteLine("Vänligen ange minst tre ord");
-                            messageToSplit = Console.ReadLine();
-                        }
-                        Console.WriteLine(words.ElementAt(2));
+                        if (words.Length < wordCount)
+                            GetStringInput($"meddelande med minst {wordCount} ord: ");
+                        else PrintMessage(words.ElementAt(wordCount - 1));
                         break;
                     default:
-                        Console.WriteLine("Vänligen ange ett giltigt alternativ");
+                        PrintMessage("Vänligen ange ett giltigt alternativ");
                         break;
                 }
             }
-            while (true);
+            while (isActive);
         }
-
-     static bool IsValidStringInput (string input)
+    private static uint GetUIntInput(string prompt)
+    {
+        do
         {
-            if (string.IsNullOrWhiteSpace(input))
-                return false;
-            else 
-                return true;
+            string input = GetStringInput(prompt);
+            if (uint.TryParse(input, out uint result))
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine($"Vänligen ange ett giltigt {prompt}");
+            }
         }
+        while (true);    
+    }
 
+    private static string GetStringInput(string prompt)
+    { 
+        bool isCorrect = false;
+        string input;
+
+        do
+        {
+            Console.WriteLine(prompt);
+            input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine($"Vänligen ange ett giltigt {prompt}");
+                input = Console.ReadLine();
+            }
+            else
+            {
+                isCorrect = true;
+            }
+        } while (!isCorrect);
+
+        return input;
+    }
+
+    private static void PrintThePrice(uint age)
+    {
+        if (age > 100 || age < 5) Console.WriteLine("Gratis tillträde");
+        else if (age < 20) Console.WriteLine("Ungdomspris: 80kr");
+        else if (age > 64) Console.WriteLine("Pensionärspris: 90kr");
+        else Console.WriteLine("Standardpris: 120kr");
+    }
+
+    private static void PrintMessage(string message, int printCount = 1)
+        { 
+            if (printCount > 1 )
+            { 
+                for (int i = 0; i < printCount; i++)
+                {
+                    Console.Write(message);
+                }
+            }
+            else 
+                { 
+                Console.WriteLine(message); 
+            } 
+        }
 
     }
 }
